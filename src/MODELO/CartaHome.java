7e176992808ee95fede95a885b47d1e78bb8e58b
danielmr <danielmarchantero2001@gmail.com>
2022-03-25 22:54:02 +0100

    Crear Proyecto

---
+++ src/MODELO/CartaHome.java
@@ -0,0 +1,116 @@
+package MODELO;
+// Generated 25 mar. 2022 22:49:44 by Hibernate Tools 5.4.32.Final
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Carta.
+ * @see MODELO.Carta
+ * @author Hibernate Tools
+ */
+public class CartaHome {
+
+	private static final Logger logger = Logger.getLogger(CartaHome.class.getName());
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext().lookup("SessionFactory");
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Carta transientInstance) {
+		logger.log(Level.INFO, "persisting Carta instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			logger.log(Level.INFO, "persist successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Carta instance) {
+		logger.log(Level.INFO, "attaching dirty Carta instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			logger.log(Level.INFO, "attach successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Carta instance) {
+		logger.log(Level.INFO, "attaching clean Carta instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			logger.log(Level.INFO, "attach successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Carta persistentInstance) {
+		logger.log(Level.INFO, "deleting Carta instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			logger.log(Level.INFO, "delete successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "delete failed", re);
+			throw re;
+		}
+	}
+
+	public Carta merge(Carta detachedInstance) {
+		logger.log(Level.INFO, "merging Carta instance");
+		try {
+			Carta result = (Carta) sessionFactory.getCurrentSession().merge(detachedInstance);
+			logger.log(Level.INFO, "merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "merge failed", re);
+			throw re;
+		}
+	}
+
+	public Carta findById(int id) {
+		logger.log(Level.INFO, "getting Carta instance with id: " + id);
+		try {
+			Carta instance = (Carta) sessionFactory.getCurrentSession().get("MODELO.Carta", id);
+			if (instance == null) {
+				logger.log(Level.INFO, "get successful, no instance found");
+			} else {
+				logger.log(Level.INFO, "get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Carta instance) {
+		logger.log(Level.INFO, "finding Carta instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria("MODELO.Carta")
+					.add(Example.create(instance)).list();
+			logger.log(Level.INFO, "find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "find by example failed", re);
+			throw re;
+		}
+	}
+}
